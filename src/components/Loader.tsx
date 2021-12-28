7d8c505c98858d850e9b546e84365698a095b3dd
eruj22 <prci.prnaver2@gmail.com>
2021-12-28 05:27:28 +0100

    finished success page

---
+++ src/components/Loader.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="loader__container">
+      <div className="loader">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import Loader from "../components/Loader";
 
 function Success() {
+  const [getGifs, setGetGifs] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const randomNumber = Math.floor(Math.random() * 50);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.giphy.com/v1/gifs/search?api_key=KeTn0RgXZQF8EDkUGgQmSaJYuWPEz5mI&q=barber"
+      )
+      .then((response) => {
+        setGetGifs(response.data.data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <section className="success">
-      <h1 className="success__title">success</h1>
+      <div className="container">
+        <h1 className="success__title">success</h1>
+
+        <img
+          className="success__image"
+          src={getGifs[randomNumber]?.images.fixed_height.url}
+          alt=""
+        />
+      </div>
     </section>
   );
 }
